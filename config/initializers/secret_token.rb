cf255aaa73f2a7ace4d6f4f4020ad582c4a5d082
Partouche <natanel.partouche@gmail.com>
2012-08-05 18:18:16 +0100

    first upload umpty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VitrineTonavenirNet::Application.config.secret_token = '736dc3c22716c290b95357b2dfc21d334686115df5ca63ae3bd8c3f7d1d6a8b85b70de7e5cb28b171feb62d520f43ae578698e6d3fe4ff74f05fafbed67f13f4'
